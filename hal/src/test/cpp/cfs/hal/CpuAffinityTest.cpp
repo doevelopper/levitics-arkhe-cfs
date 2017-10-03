b1a3d4c6b12264bad193ea28a2e7ae7da0492e92
Doe V. Looper <happyman@hotmail.fr>
2017-10-04 00:25:40 +0200

    WIP

--- hal/src/test/cpp/cfs/hal/CpuAffinityTest.cpp
+++ hal/src/test/cpp/cfs/hal/CpuAffinityTest.cpp
@@ -3,3 +3,18 @@
 
 CpuAffinityTest::CpuAffinityTest() {
 }
+
+void CpuAffinityTest::SetUp ()
+{
+//    testObject = new CpuAffinity();
+}
+
+void CpuAffinityTest::TearDown ()
+{
+//    delete testObject;
+}
+
+TEST_F(CpuAffinityTest , testBlaBlaBla)
+{
+//    type ts = testObject->methodCall();
+}
--- hal/src/test/cpp/cfs/hal/CpuAffinityTest.hpp
+++ hal/src/test/cpp/cfs/hal/CpuAffinityTest.hpp
@@ -1,13 +1,17 @@
 
 #ifndef CFS_HAL_CPUAFFINITYTEST_HPP
 #define CFS_HAL_CPUAFFINITYTEST_HPP
-
-class CpuAffinityTest
+#include <gtest/gtest.h>
+#include <cfs/hal/cpu/CpuAffinity.hpp>
+class CpuAffinityTest : public ::testing::Test
 {
     public:
 
         CpuAffinityTest();
         virtual ~CpuAffinityTest() = default;
+
+        virtual void SetUp ();
+        virtual void TearDown ();
 
     private:
 };
